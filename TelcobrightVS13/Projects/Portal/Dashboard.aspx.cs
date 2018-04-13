4e40635b83f4f97d2d9493bf4685cca45df297c3
sabbirdev <sabbirdev@gmail.com>
2018-04-13 08:07:09 +0600

    end of the day

--- TelcobrightVS13/Projects/Portal/Dashboard.aspx.cs
+++ TelcobrightVS13/Projects/Portal/Dashboard.aspx.cs
@@ -46,8 +46,8 @@ public partial class DashboardAspx : Page
         List<DashBoard.ErrorCalls> ec = new List<DashBoard.ErrorCalls>();
         using (PartnerEntities conPartner = new PartnerEntities())
         {
-            ec = conPartner.Database.SqlQuery<DashBoard.ErrorCalls>(@"select field4 as ErrorReason, count(*) as NumberOfCalls
-                                                from cdrerror group by field4").ToList();
+            ec = conPartner.Database.SqlQuery<DashBoard.ErrorCalls>(@"select errorCode as ErrorReason, count(*) as NumberOfCalls
+                                                from cdrerror group by errorCode").ToList();
             this.HyperLinkError.Text = ec.Select(c=>c.NumberOfCalls).Sum() + " Calls in Error";
             this.GridViewError.DataSource = ec;
             this.GridViewError.DataBind();
