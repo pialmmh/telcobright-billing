0ab0d7fb7c06d78e4f443aa3145c3ab7a5e96c3c
Your Name <you@example.com>
2018-04-19 15:56:52 +0600

    19April2018_ssh25janremoved

---
+++ TelcobrightVS13/Projects/Mediation/Automation/CliCommandSequence.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using TelcobrightMediation.Accounting;
+
+namespace TelcobrightMediation.Automation
+{
+    public class CliCommandSequence
+    {
+        public List<SingleCliCommand> commands { get; set; }
+    }
+
+}
