8a8cee54c01b170ceb2b3f60aa8f0d9e5ce95ef7
Your Name <you@example.com>
2018-05-03 02:42:26 +0600

    03May2018_5

--- TelcobrightVS13/Projects/Mediation/Cdr/cdrExtFactory.cs
+++ TelcobrightVS13/Projects/Mediation/Cdr/cdrExtFactory.cs
@@ -36,7 +36,7 @@ namespace TelcobrightMediation.Cdr
                 case CdrNewOldType.NewCdr:
                     if (partialCdrContainer.NewCdrEquivalent.FinalRecord != 1)
                         throw new Exception("Partial cdr equivalent instance must have final record set to 1.");
-                    if (partialCdrContainer.NewCdrEquivalent.PartialFlag != 1)
+                    if (partialCdrContainer.NewCdrEquivalent.PartialFlag >0)
                         throw new Exception("Partial cdr equivalent instance must have partial flag set to 1.");
                     return new CdrExt(partialCdrContainer.NewCdrEquivalent, CdrNewOldType.NewCdr)
                     {
