2a3dd3f6dcc55381d1e09e2e6d18be910a3c3f08
Wahid Sabbir <sabbirdev@gmail.com>
2018-07-06 14:04:23 +0600

    6July2018_2

--- TelcobrightVS13/Projects/UtilInstallConfig/btel/BtelDirectorySetting.cs
+++ TelcobrightVS13/Projects/UtilInstallConfig/btel/BtelDirectorySetting.cs
@@ -29,14 +29,14 @@ namespace InstallConfig
             //***FILE LOCATIONS**********************************************
             //local/vault1: all app servers will use same local file location
             //the object "vault" will have a copy of below object for each app servers with server id as key and location as dictionary value
-            FileLocation vaultPltDhkDl = new FileLocation()
+            FileLocation vaultBtelZteDhk = new FileLocation()
             {
-                Name = "Vault.PltDhkDL",//this is refered in ne table, name MUST start with "Vault"
+                Name = "Vault.BtelZteDhk",//this is refered in ne table, name MUST start with "Vault"
                 LocationType = "vault",//locationtype always lowercase
                 OsType = "windows",
                 PathSeparator = @"\",
                 ServerIp = "",
-                StartingPath = "C:/telcobright/Vault/Resources/CDR/platinum/PltDhkDL",
+                StartingPath = "C:/telcobright/Vault/Resources/CDR/btel/BtelZteDhk",
                 User = "",
                 Pass = "",
             };
@@ -46,8 +46,8 @@ namespace InstallConfig
                 LocationType = "ftp",
                 OsType = "windows",
                 PathSeparator = "/",
-                ServerIp = "192.168.2.216",
-                StartingPath = "Resources/CDR/platinum/PltDhkDL",
+                ServerIp = "10.100.201.22",
+                StartingPath = "Resources/CDR/btel/BtelZteDhk",
                 User = "ftpuser",
                 Pass = "Takay1takaane",
             };
@@ -57,8 +57,8 @@ namespace InstallConfig
                 LocationType = "ftp",
                 OsType = "windows",
                 PathSeparator = "/",
-                ServerIp = "192.168.2.224",
-                StartingPath = "Resources/CDR/platinum/PltDhkDL",
+                ServerIp = "10.100.201.23",
+                StartingPath = "Resources/CDR/btel/BtelZteDhk",
                 User = "ftpuser",
                 Pass = "Takay1takaane",
                 Skip = true
@@ -67,92 +67,89 @@ namespace InstallConfig
             List<FileLocation> ftpLocations = new List<FileLocation>();
             ftpLocations.Add(appServerFtp1);
             ftpLocations.Add(appServerFtp2);
-            Vault pltDhkDLvault = new Vault("Vault.PltDhkDL",tbc , ftpLocations);
-            pltDhkDLvault.LocalLocation = new SyncLocation(vaultPltDhkDl.Name) { FileLocation = vaultPltDhkDl };//don't pass this to constructor and set there, causes problem in json serialize
-            directorySettings.Vaults.Add(pltDhkDLvault);
-            FileLocation pltDhkDl = new FileLocation()
+            Vault BtelZteDhkvault = new Vault("Vault.BtelZteDhk",tbc , ftpLocations);
+            BtelZteDhkvault.LocalLocation = new SyncLocation(vaultBtelZteDhk.Name) { FileLocation = vaultBtelZteDhk };//don't pass this to constructor and set there, causes problem in json serialize
+            directorySettings.Vaults.Add(BtelZteDhkvault);
+            FileLocation BtelZteDhk = new FileLocation()
             {
-                Name = "PltDhkDL",
-                LocationType = "ftp",
+                Name = "BtelZteDhk",
+                LocationType = "sftp",
                 OsType = "linux",
                 PathSeparator = "/",
-                StartingPath = "/cdr/ICDR/primary",
+                StartingPath = "/home/zxss10_bsvr/data/bfile/bill/zsmart_media_bak",
                 Sftphostkey = "",
-                ServerIp = "10.10.10.10",
-                User = "ics",
-                Pass = "icsveraz",
+                ServerIp = "10.133.34.12",
+                User = "igwbill",
+                Pass = "igw123",
                 ExcludeBefore = new DateTime(2015, 6, 26, 0, 0, 0),
                 IgnoreZeroLenghFile = 1
             };
-
-            //FileLocation S3_2 = new FileLocation()
-            //{
-            //    Name = "S3_2",
-            //    locationType = "ftp",
-            //    OSType = "windows",
-            //    pathSeparator = "/",
-            //    startingPath = "/oldcdr",
-            //    sftphostkey = "ssh-rsa 1024 0d:fd:ac:4a:67:05:e9:76:ef:a0:d2:c0:f9:1a:55:c1",
-            //    server = "172.16.16.242",
-            //    user = "ftpuser",
-            //    pass = "Takay1takaane",
-            //    ExcludeBefore = new DateTime(2015, 6, 26, 0, 0, 0),
-            //    IgnoreZeroLenghFile = 1
-            //};
-
-            FileLocation fileArchive1 = new FileLocation()//raw cdr archive
+            FileLocation fileArchive1Zip = new FileLocation()//raw cdr archive
             {
-                Name = "FileArchive1",
+                Name = "FileArchive1Zip",
                 LocationType = "ftp",
                 OsType = "windows",
                 PathSeparator = @"/",//backslash didn't work with winscp
-                StartingPath = @"/archive",
-                ServerIp = "192.168.2.225", //server = "172.16.16.242",
-                User = "ftpuser",
-                Pass = "Takay1takaane",
+                StartingPath = @"/RAW_DATA/IGW_CDR",
+                ServerIp = "10.100.201.20", //server = "172.16.16.242",
+                User = "cdr",
+                Pass = "cdr13531",
                 IgnoreZeroLenghFile = 1
             };
-            FileLocation fileArchiveIof = new FileLocation()//raw cdr archive
+            FileLocation fileArchive2 = new FileLocation()//raw cdr archive
             {
-                Name = "FileArchiveIOF",
+                Name = "FileArchive2",
                 LocationType = "ftp",
                 OsType = "linux",
                 PathSeparator = "/",
-                StartingPath = @"/",
-                ServerIp = "172.22.21.11", //server = "172.16.16.242",
-                User = "platinum",
-                Pass = "usr_platinum3!2",
+                StartingPath = @"/medi/data/igw_src/igw_src_bk",
+                ServerIp = "10.100.201.10", //server = "172.16.16.242",
+                User = "medi",
+                Pass = "medi",
+                IgnoreZeroLenghFile = 1,
+            };
+            FileLocation fileArchiveIof = new FileLocation()//raw cdr archive
+            {
+                Name = "FileArchiveIof",
+                LocationType = "ftp",
+                OsType = "linux",
+                PathSeparator = "/",
+                StartingPath = @"/medi/data/igw_src/igw_src_bk",
+                ServerIp = "10.100.201.13",
+                User = "iofcdr",
+                Pass = "blt#.45",
                 IgnoreZeroLenghFile = 1,
             };
             //add locations to directory settings
-            directorySettings.FileLocations.Add(vaultPltDhkDl.Name, vaultPltDhkDl);
+            directorySettings.FileLocations.Add(vaultBtelZteDhk.Name, vaultBtelZteDhk);
             directorySettings.FileLocations.Add(appServerFtp1.Name, appServerFtp1);
             directorySettings.FileLocations.Add(appServerFtp2.Name, appServerFtp2);
-            directorySettings.FileLocations.Add(pltDhkDl.Name, pltDhkDl);
+            directorySettings.FileLocations.Add(BtelZteDhk.Name, BtelZteDhk);
             
-            directorySettings.FileLocations.Add(fileArchive1.Name, fileArchive1);
+            directorySettings.FileLocations.Add(fileArchive1Zip.Name, fileArchive1Zip);
+            directorySettings.FileLocations.Add(fileArchive2.Name, fileArchive2);
             directorySettings.FileLocations.Add(fileArchiveIof.Name, fileArchiveIof);
 
 
             //sync pair platinum:Vault
-            SyncPair pltDhkDlVault = new SyncPair("PltDhkDL:Vault")
+            SyncPair BtelZteDhkVault = new SyncPair("BtelZteDhk:Vault")
             {
                 SkipSourceFileListing = false,
-                SrcSyncLocation = new SyncLocation("PltDhkDL")
+                SrcSyncLocation = new SyncLocation("BtelZteDhk")
                 {
-                    FileLocation = pltDhkDl,
+                    FileLocation = BtelZteDhk,
                     DescendingFileListByFileName = tbc.CdrSetting.DescendingOrderWhileListingFiles
                 },
-                DstSyncLocation = new SyncLocation("Vault_PltDhkDL")
+                DstSyncLocation = new SyncLocation("Vault_BtelZteDhk")
                 {
-                    FileLocation = vaultPltDhkDl
+                    FileLocation = vaultBtelZteDhk
                 },
                 SrcSettings = new SyncSettingsSource()
                 {
                     SecondaryDirectory = "Downloaded",
-                    ExpFileNameFilter = new SpringExpression(@"Name.StartsWith('icdr')
+                    ExpFileNameFilter = new SpringExpression(@"Name.StartsWith('IGW')
                                                                 and
-                                                                (Name.EndsWith('.0') or Name.EndsWith('.1'))
+                                                                (Name.EndsWith('.DAT'))
                                                                 and Length>0")
                 },
                 DstSettings = new SyncSettingsDest()
@@ -166,17 +163,17 @@ namespace InstallConfig
 
 
             //sync pair Vault_S3:FileArchive1
-            SyncPair vaultS3FileArchive1 = new SyncPair("Vault:FileArchive1")
+            SyncPair vaultFileArchive1Zip = new SyncPair("Vault:FileArchive1Zip")
             {
                 SkipCopyingToDestination = false,
                 SkipSourceFileListing = true,
-                SrcSyncLocation = new SyncLocation("Vault_PltDhkDL")
+                SrcSyncLocation = new SyncLocation("Vault_BtelZteDhk")
                 {
-                    FileLocation = vaultPltDhkDl
+                    FileLocation = vaultBtelZteDhk
                 },
-                DstSyncLocation = new SyncLocation("FileArchive1")
+                DstSyncLocation = new SyncLocation("FileArchive1Zip")
                 {
-                    FileLocation = fileArchive1
+                    FileLocation = fileArchive1Zip
                 },
                 SrcSettings = new SyncSettingsSource()
                 {
@@ -191,20 +188,44 @@ namespace InstallConfig
                     SubDirRule = new SyncSettingsDstSubDirectoryRule
                     (
                         DateWiseSubDirCreationType.ByFileName,
-                        new SpringExpression(@"Name.Substring(16,8)"), //"S3_2_" is appended at vault
-                        "yyyyMMdd",
-                        true
+                        new SpringExpression(@"Name.Substring(3,8)"), //"S3_2_" is appended at vault
+                        "yyyyMMdd", true
                     )
                 }
             };
-            //sync pair Vault_S3:IOF
-            SyncPair vaultS3Iof = new SyncPair("Vault:IOF")
+            SyncPair vaultFileArchive2 = new SyncPair("Vault:FileArchive2")
             {
                 SkipCopyingToDestination = false,
                 SkipSourceFileListing = true,
-                SrcSyncLocation = new SyncLocation("Vault_S3")
+                SrcSyncLocation = new SyncLocation("Vault_BtelZteDhk")
                 {
-                    FileLocation = vaultPltDhkDl
+                    FileLocation = vaultBtelZteDhk
+                },
+                DstSyncLocation = new SyncLocation("FileArchive2")
+                {
+                    FileLocation = fileArchive2
+                },
+                SrcSettings = new SyncSettingsSource()
+                {
+                    SecondaryDirectory = "Downloaded",
+                    ExpFileNameFilter = null,
+                },
+                DstSettings = new SyncSettingsDest()
+                {
+                    FileExtensionForSafeCopyWithTempFile = ".tmp",
+                    Overwrite = true,
+                    ExpDestFileName = new SpringExpression(""),
+                    CompressionType = CompressionType.None,
+                    SubDirRule = null
+                }
+            };
+            SyncPair vaultIof = new SyncPair("Vault:IOF")
+            {
+                SkipCopyingToDestination = false,
+                SkipSourceFileListing = true,
+                SrcSyncLocation = new SyncLocation("Vault_BtelZteDhk")
+                {
+                    FileLocation = vaultBtelZteDhk
                 },
                 DstSyncLocation = new SyncLocation("IOF")
                 {
@@ -225,9 +246,10 @@ namespace InstallConfig
             };
 
             //add sync pairs to directory config
-            directorySettings.SyncPairs.Add(pltDhkDlVault.Name, pltDhkDlVault);
-            directorySettings.SyncPairs.Add(vaultS3FileArchive1.Name, vaultS3FileArchive1);
-            directorySettings.SyncPairs.Add(vaultS3Iof.Name, vaultS3Iof);
+            directorySettings.SyncPairs.Add(BtelZteDhkVault.Name, BtelZteDhkVault);
+            directorySettings.SyncPairs.Add(vaultFileArchive1Zip.Name, vaultFileArchive1Zip);
+            directorySettings.SyncPairs.Add(vaultFileArchive2.Name, vaultFileArchive2);
+            directorySettings.SyncPairs.Add(vaultIof.Name, vaultIof);
             //load the syncpairs in dictioinary, first by source
             foreach (SyncPair sp in directorySettings.SyncPairs.Values)
             {
@@ -248,7 +270,7 @@ namespace InstallConfig
             //add archive locations to CdrSettings
             tbc.CdrSetting.BackupSyncPairNames = new List<string>
             {
-                vaultS3FileArchive1.Name, vaultS3Iof.Name
+                vaultFileArchive1Zip.Name, vaultIof.Name
             };
             tbc.CdrSetting.DisableCdrPostProcessingJobCreationForAutomation = true;
         }
