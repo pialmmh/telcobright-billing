9b25659b6ccfc4cf3b05020de2f4c98abf675a53
Wahid Sabbir <sabbirdev@gmail.com>
2018-07-20 16:02:15 +0600

    work in progress

---
+++ TelcobrightVS13/Projects/Portal/config/GeneratedInvoices.aspx
@@ -0,0 +1,62 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="GeneratedInvoices.aspx.cs" Inherits="PortalApp.config.GeneratedInvoices" MasterPageFile="~/Site.master" %>
+<asp:Content ID="HeaderContent" runat="server" ContentPlaceHolderID="HeadContent">
+   
+    <script src="../Scripts/moment.js" type="text/javascript"></script>
+
+    <%--Page Load and Other Server Side Asp.net scripts--%>
+       
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="MainContent" Runat="Server">
+    <ajaxToolkit:ToolkitScriptManager ID="ToolkitScriptManager1" runat="server" />
+    <div>
+        <span style="color: black;"><b>Generated Invoices:</b></span><br/>
+        <asp:UpdatePanel runat="server">
+            <ContentTemplate>
+                <asp:GridView ID="gvInvoice" runat="server" AutoGenerateColumns="False" DataKeyNames="PartnerId"
+                    CellPadding="4" ForeColor="#333333" GridLines="Vertical"
+                    Font-Size="9pt" BorderColor="Silver" BorderStyle="Solid"
+                    OnRowEditing="gvInvoice_OnRowEditing" OnRowDataBound="gvInvoice_OnRowDataBound"
+                    OnRowUpdating="gvInvoice_OnRowUpdating" OnRowCancelingEdit="gvInvoice_OnRowCancelingEdit">
+                    <AlternatingRowStyle BackColor="#f2f2f2" ForeColor="#284775"></AlternatingRowStyle>
+                    <HeaderStyle BackColor="#5D7B9D" Font-Bold="True" ForeColor="White" />
+                    <PagerStyle BackColor="#284775" ForeColor="White" HorizontalAlign="Center" />
+                    <RowStyle BackColor="white" ForeColor="#333333" />
+                    <Columns>
+                        <asp:templatefield>
+                            <itemtemplate>
+                                <asp:linkbutton id="btnEdit" runat="server" commandname="Edit" text="Edit" />
+                            </itemtemplate>
+                            <edititemtemplate>
+                                <asp:linkbutton id="btnUpdate" runat="server" commandname="Update" text="Update" />
+                                <asp:linkbutton id="btnCancel" runat="server" commandname="Cancel" text="Cancel" />
+                            </edititemtemplate>
+                        </asp:templatefield>                        
+                        <asp:boundfield datafield="PartnerName" headertext="Partner"/>
+                        <asp:boundfield datafield="ServiceAccount" headertext="Service Account"/>
+                        <asp:TemplateField HeaderText="Time Zone" SortExpression="TimeZone" ItemStyle-Wrap="false">
+                            <ItemTemplate>
+                                <asp:Label ID="lblTimeZone" runat="server" Text=""></asp:Label>
+                            </ItemTemplate>
+                            <EditItemTemplate>
+                                <asp:DropDownList ID="ddlistTimeZone" runat="server" AutoPostBack="false" Enabled="True"/>
+                            </EditItemTemplate>
+                        </asp:TemplateField>
+                        <asp:boundfield datafield="StartDateTime" headertext="From" dataformatstring="{0:yyyy-MM-dd HH:mm:ss}" />
+                        <asp:boundfield datafield="EndDateTime" headertext="Till" dataformatstring="{0:yyyy-MM-dd HH:mm:ss}" />
+                        <asp:boundfield datafield="Amount" headertext="Amount" DataFormatString="{0:n2}">
+                            <ItemStyle HorizontalAlign="Right" />
+                        </asp:boundfield>
+                        <asp:TemplateField>
+                            <ItemTemplate>
+                                <asp:CheckBox runat="server" id="cbSelect" Checked="False"/>
+                            </ItemTemplate>
+                        </asp:TemplateField>
+                    </Columns>
+                </asp:GridView>
+            </ContentTemplate>
+            <Triggers>
+                
+            </Triggers>
+        </asp:UpdatePanel>
+    </div>
+</asp:Content>
---
+++ TelcobrightVS13/Projects/Portal/config/GeneratedInvoices.aspx.cs
@@ -0,0 +1,354 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Itenso.TimePeriod;
+using LibraryExtensions;
+using MediationModel;
+using MySql.Data.MySqlClient;
+using Newtonsoft.Json;
+using TelcobrightMediation;
+using TelcobrightMediation.Accounting;
+using TelcobrightMediation.Config;
+using Itenso.TimePeriod;
+namespace PortalApp.config
+{
+    public partial class GeneratedInvoices : System.Web.UI.Page
+    {
+        private static TelcobrightConfig Tbc { get; set; }
+        private static int DefaultTimeZoneId = 3251;
+        private static int GmtOffset = 21600;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                Tbc = PageUtil.GetTelcobrightConfig();
+                using (PartnerEntities context = new PartnerEntities())
+                {
+
+                    List<partner> allPartners = context.partners.OrderBy(i => i.PartnerName).ToList();
+                    this.Session["sesAllPartners"] = allPartners;
+                    List<account> allAccounts = context.accounts.ToList();
+
+                    List<timezone> tz = context.timezones.Include("zone.country")
+                        .OrderBy(o => o.zone.country.country_name).ToList();
+                    this.Session["sesAllTimeZones"] = tz;
+
+                    List<KeyValuePair<Regex, string>> serviceAliases = Tbc.ServiceAliasesRegex;
+                    List<InvoiceDataCollector> summaryForInvoiceGenerations =
+                        new List<InvoiceDataCollector>();
+                    List<BillingRule> billingRules = context.jsonbillingrules.ToList()
+                        .Select(c => JsonConvert.DeserializeObject<BillingRule>(c.JsonExpression)).ToList();
+                    List<long> accountIds = context.accounts.Where(x => x.isCustomerAccount == 1 && x.isBillable == 1)
+                        .Select(x => x.id).ToList();
+                    List<acc_ledger_summary> accLedgerSummaries =
+                        context.acc_ledger_summary.Where(x => accountIds.Contains(x.idAccount) && x.AMOUNT != 0)
+                            .ToList();
+                    bool isAlreadyExists = false;
+                    foreach (acc_ledger_summary ledgerSummary in accLedgerSummaries)
+                    {
+                        var serviceGroup = context.accounts.First(x => x.id == ledgerSummary.idAccount).serviceGroup;
+                        var idBillingRule = context.billingruleassignments.First(x => x.idServiceGroup == serviceGroup)
+                            .idBillingRule;
+                        BillingRule billingRule = billingRules.First(x => x.Id == idBillingRule);
+                        TimeRange timeRange =
+                            billingRule.GetBillingCycleByBillableItemsDate(ledgerSummary.transactionDate);
+                        account account = allAccounts.First(x => x.id == ledgerSummary.idAccount);
+                        partner partner = allPartners.First(x => x.idPartner == account.idPartner);
+
+                        if (summaryForInvoiceGenerations.Count > 0)
+                        {
+                            InvoiceDataCollector cycle = summaryForInvoiceGenerations
+                                .FirstOrDefault(x => x.AccountId == ledgerSummary.idAccount &&
+                                                     x.PartnerId == partner.idPartner &&
+                                                     x.StartDateTime.Equals(timeRange.Start) &&
+                                                     x.EndDateTime.Equals(timeRange.End));
+                            if (cycle != null)
+                            {
+                                cycle.Amount += ledgerSummary.AMOUNT;
+                                cycle.InvoiceDates.Add(ledgerSummary.transactionDate);
+                                isAlreadyExists = true;
+                            }
+                        }
+
+                        if (!isAlreadyExists)
+                        {
+                            InvoiceDataCollector invoiceGeneration = new InvoiceDataCollector();
+                            invoiceGeneration.PartnerId = partner.idPartner;
+                            invoiceGeneration.PartnerName = partner.PartnerName;
+                            invoiceGeneration.AccountId = account.id;
+                            invoiceGeneration.AccountName = account.accountName;
+                            invoiceGeneration.StartDateTime = timeRange.Start;
+                            invoiceGeneration.EndDateTime = timeRange.End;
+                            invoiceGeneration.Amount = ledgerSummary.AMOUNT;
+                            invoiceGeneration.TimeZone = DefaultTimeZoneId;
+                            invoiceGeneration.GmtOffset = GmtOffset;
+                            invoiceGeneration.InvoiceDates.Add(ledgerSummary.transactionDate);
+
+                            foreach (var kv in serviceAliases)
+                            {
+                                var regex = kv.Key;
+                                if (regex.Matches(invoiceGeneration.AccountName).Count > 0)
+                                {
+                                    invoiceGeneration.ServiceAccount = kv.Value;
+                                    break;
+                                }
+                            }
+
+                            summaryForInvoiceGenerations.Add(invoiceGeneration);
+                        }
+                    }
+
+                    BindingList<InvoiceDataCollector> invoiceGenerations =
+                        new BindingList<InvoiceDataCollector>(summaryForInvoiceGenerations);
+                    this.Session["igInvoiceGenList"] = invoiceGenerations;
+                    gvInvoice.DataSource = invoiceGenerations;
+                    gvInvoice.DataBind();
+
+                }
+            }
+        }
+
+        protected void gvInvoice_OnRowEditing(object sender, GridViewEditEventArgs e)
+        {
+            gvInvoice.EditIndex = e.NewEditIndex;
+            BindingList<InvoiceDataCollector> invoiceGenerations =
+                (BindingList<InvoiceDataCollector>) this.Session["igInvoiceGenList"];
+            gvInvoice.DataSource = invoiceGenerations;
+            gvInvoice.DataBind();
+        }
+
+        protected void gvInvoice_OnRowDataBound(object sender, GridViewRowEventArgs e)
+        {
+            if (e.Row.RowType == DataControlRowType.DataRow)
+            {
+                if ((e.Row.RowState & DataControlRowState.Edit) > 0)
+                {
+                    DropDownList ddlTimeZone = (DropDownList) e.Row.FindControl("ddlistTimeZone");
+                    if (DataBinder.Eval(e.Row.DataItem, "TimeZone") != null)
+                    {
+                        if (this.Session["sesAllTimeZones"] != null)
+                        {
+                            List<timezone> allTimeZones = (List<timezone>) this.Session["sesAllTimeZones"];
+                            foreach (timezone t in allTimeZones)
+                            {
+                                string zoneName = t.zone.country.country_name + " " + t.offsetdesc + " [" +
+                                                  t.zone.zone_name + "]";
+                                ddlTimeZone.Items.Add(new ListItem(zoneName, t.id.ToString()));
+                            }
+                        }
+                        int idTimeZone = int.Parse(DataBinder.Eval(e.Row.DataItem, "TimeZone").ToString());
+                        ddlTimeZone.SelectedValue = idTimeZone.ToString();
+                    }
+                }
+                else
+                {
+                    Label thisLabel = (Label) e.Row.FindControl("lblTimeZone");
+                    if (DataBinder.Eval(e.Row.DataItem, "TimeZone") != null)
+                    {
+                        int idTimeZone = int.Parse(DataBinder.Eval(e.Row.DataItem, "TimeZone").ToString());
+                        if (idTimeZone > 0)
+                        {
+                            if (this.Session["sesAllTimeZones"] != null)
+                            {
+                                List<timezone> allTimeZones = (List<timezone>) this.Session["sesAllTimeZones"];
+                                string tzName = (from c in allTimeZones
+                                    where c.id == idTimeZone
+                                    select c.zone.country.country_name + " " + c.offsetdesc + " [" + c.zone.zone_name +
+                                           "]").First();
+                                thisLabel.Text = tzName;
+                            }
+                        }
+
+                    }
+                }
+            }
+        }
+
+        protected void btnAddInvoiceRow_OnClick(object sender, EventArgs e)
+        {
+            List<timezone> allTimeZones = (List<timezone>) this.Session["sesAllTimeZones"];
+            BindingList<InvoiceDataCollector> invoiceGenerations =
+                (BindingList<InvoiceDataCollector>) this.Session["igInvoiceGenList"];
+            InvoiceDataCollector ledgerSummary = new InvoiceDataCollector();
+            ledgerSummary.PartnerId = Convert.ToInt32(ddlistPartner.SelectedValue);
+            ledgerSummary.PartnerName = ddlistPartner.SelectedItem.Text;
+            ledgerSummary.ServiceAccount = ddlistServiceAccount.Text;
+            ledgerSummary.StartDateTime = Convert.ToDateTime(txtDate.Text);
+            ledgerSummary.EndDateTime = Convert.ToDateTime(txtDate1.Text);
+            ledgerSummary.TimeZone = Convert.ToInt32(ddlistTimeZone.SelectedValue);
+            ledgerSummary.GmtOffset = allTimeZones.First(x => x.id == ledgerSummary.TimeZone).gmt_offset;
+            ledgerSummary.InvoiceDates.Add(ledgerSummary.StartDateTime);
+            invoiceGenerations.Add(ledgerSummary);
+            this.Session["igInvoiceGenList"] = invoiceGenerations;
+            gvInvoice.DataSource = invoiceGenerations;
+            gvInvoice.DataBind();
+        }
+
+        protected void gvInvoice_OnRowUpdating(object sender, GridViewUpdateEventArgs e)
+        {
+            int index = e.RowIndex;
+            DropDownList ddlTimeZone = gvInvoice.Rows[index].FindControl("ddlistTimeZone") as DropDownList;
+            if (ddlTimeZone != null)
+            {
+                gvInvoice.EditIndex = -1;
+                List<timezone> allTimeZones = (List<timezone>) this.Session["sesAllTimeZones"];
+                BindingList<InvoiceDataCollector> invoiceGenerations =
+                    (BindingList<InvoiceDataCollector>) this.Session["igInvoiceGenList"];
+                InvoiceDataCollector editRow =
+                    invoiceGenerations.First(x => x.PartnerId == Convert.ToInt32(e.Keys[0]));
+                editRow.TimeZone = Convert.ToInt32(ddlTimeZone.SelectedValue);
+                editRow.GmtOffset = allTimeZones.First(x => x.id == editRow.TimeZone).gmt_offset;
+                this.Session["igInvoiceGenList"] = invoiceGenerations;
+                gvInvoice.DataSource = invoiceGenerations;
+                gvInvoice.DataBind();
+            }
+        }
+
+        protected void gvInvoice_OnRowCancelingEdit(object sender, GridViewCancelEditEventArgs e)
+        {
+            gvInvoice.EditIndex = -1;
+            BindingList<InvoiceDataCollector> invoiceGenerations =
+                (BindingList<InvoiceDataCollector>) this.Session["igInvoiceGenList"];
+            gvInvoice.DataSource = invoiceGenerations;
+            gvInvoice.DataBind();
+        }
+
+        protected void btnGenerateInvoice_OnClick(object sender, EventArgs e)
+        {
+            try
+            {
+                int batchSizeForJobSegments = 10000;
+                BindingList<InvoiceDataCollector> boundRowsForInvoiceGeneration =
+                    (BindingList<InvoiceDataCollector>) this.Session["igInvoiceGenList"];
+                List<job> invoicingJobs = new List<job>();
+                if (this.gvInvoice.Rows.Count <= 0)
+                {
+                    throw new Exception("No row selected for invoice generation.");
+                }
+                using (PartnerEntities context = new PartnerEntities())
+                {
+                    for (var index = 0; index < this.gvInvoice.Rows.Count; index++)
+                    {
+                        GridViewRow invoiceRow = this.gvInvoice.Rows[index];
+                        CheckBox cbSelect = (CheckBox) invoiceRow.FindControl("cbSelect");
+                        if (cbSelect.Checked)
+                        {
+                            var invoiceDataCollector = boundRowsForInvoiceGeneration[invoiceRow.RowIndex];
+                            if (invoiceDataCollector.EndDateTimeLocal <
+                                invoiceDataCollector.StartDateTimeLocal)
+                            {
+                                throw new Exception("EndDatetime must be >= StartDateTime for all rows, check row no:" +
+                                                    index + 1);
+                            }
+                            if (invoiceDataCollector.AccountId <= 0)
+                            {
+                                throw new Exception("AccountId must be >=0, check row no:" + index + 1);
+                            }
+                            job invoicingJob = CreateInvoiceGenerationJob(invoiceDataCollector, context,
+                                batchSizeForJobSegments);
+                            invoicingJobs.Add(invoicingJob);
+                        }
+                    }
+                    context.jobs.AddRange(invoicingJobs);
+                    context.SaveChanges();
+                    this.lblStatus.ForeColor = Color.Black;
+                    this.lblStatus.Text = invoicingJobs.Count + " invoicing job(s) created.";
+                }
+            }
+            catch (Exception exception)
+            {
+                this.lblStatus.ForeColor = Color.Red;
+                this.lblStatus.Text = exception.Message;
+            }
+
+        }
+
+
+        protected job CreateInvoiceGenerationJob(InvoiceDataCollector invoiceDataCollector,
+            PartnerEntities context, int batchSizeForJobSegment)
+        {
+            string serviceAccount = invoiceDataCollector.ServiceAccount;
+            int jobDefinition = 12;
+            int prevJobCountWithSameName =
+                context.jobs.Count(j => j.idjobdefinition == jobDefinition && j.idjobdefinition == jobDefinition);
+            if (prevJobCountWithSameName > 0)
+            {
+                serviceAccount = serviceAccount + "_" + prevJobCountWithSameName;
+            }
+            string jobName = invoiceDataCollector.PartnerName + "/" + serviceAccount
+                             + "/" + invoiceDataCollector.StartDateTimeLocal.ToMySqlStyleDateTimeStrWithoutQuote()
+                             + " to " +
+                             invoiceDataCollector.EndDateTimeLocal.ToMySqlStyleDateTimeStrWithoutQuote();
+            string sourceTable = "acc_transaction";
+            List<SqlSingleWhereClauseBuilder> singleWhereClauses = new List<SqlSingleWhereClauseBuilder>();
+            List<SqlMultiWhereClauseBuilder> multipleWhereClauses = new List<SqlMultiWhereClauseBuilder>();
+            SqlSingleWhereClauseBuilder newParam = null;
+            newParam = new SqlSingleWhereClauseBuilder(SqlWhereAndOrType.FirstBeforeAndOr);
+            newParam.Expression = "transactionTime>=";
+            newParam.ParamType = SqlWhereParamType.Datetime;
+            newParam.ParamValue = invoiceDataCollector.StartDateTimeLocal.ToMySqlStyleDateTimeStrWithoutQuote();
+            singleWhereClauses.Add(newParam);
+
+            newParam = new SqlSingleWhereClauseBuilder(SqlWhereAndOrType.And);
+            newParam.Expression = "transactionTime<=";
+            newParam.ParamType = SqlWhereParamType.Datetime;
+            newParam.ParamValue = invoiceDataCollector.EndDateTimeLocal.ToMySqlStyleDateTimeStrWithoutQuote();
+            singleWhereClauses.Add(newParam);
+
+            long glAccountId = invoiceDataCollector.AccountId;
+            newParam = new SqlSingleWhereClauseBuilder(SqlWhereAndOrType.And);
+            newParam.Expression = "glAccountId=";
+            newParam.ParamType = SqlWhereParamType.Numeric;
+            newParam.ParamValue = glAccountId.ToString();
+            singleWhereClauses.Add(newParam);
+
+            newParam = new SqlSingleWhereClauseBuilder(SqlWhereAndOrType.And);
+            newParam.Expression = "isBillable=";
+            newParam.ParamType = SqlWhereParamType.Numeric;
+            newParam.ParamValue = "1";
+            singleWhereClauses.Add(newParam);
+
+            newParam = new SqlSingleWhereClauseBuilder(SqlWhereAndOrType.And);
+            newParam.Expression = "isBilled is null or isBilled<>";
+            newParam.ParamType = SqlWhereParamType.Numeric;
+            newParam.ParamValue = "1";
+            singleWhereClauses.Add(newParam);
+
+            newParam = new SqlSingleWhereClauseBuilder(SqlWhereAndOrType.And);
+            newParam.Expression = "cancelled is null or cancelled<>";
+            newParam.ParamType = SqlWhereParamType.Numeric;
+            newParam.ParamValue = "1";
+            singleWhereClauses.Add(newParam);
+
+            BatchSqlJobParamJson jobParam = new BatchSqlJobParamJson
+            (
+                sourceTable,
+                batchSizeForJobSegment,
+                singleWhereClauses,
+                multipleWhereClauses,
+                columnExpressions: new List<string>() {"id as RowId", "transactionTime as RowDateTime"}
+            );
+
+            job newjob = new job();
+            newjob.Progress = 0;
+            newjob.idjobdefinition = 12; //invoicing job
+            newjob.Status = 6; //created
+            newjob.JobName = jobName;
+            newjob.idjobdefinition = jobDefinition;
+            newjob.CreationTime = DateTime.Now;
+            newjob.idNE = 0;
+            newjob.JobParameter = JsonConvert.SerializeObject(jobParam);
+            newjob.priority = 5;
+            return newjob;
+        }
+    }
+}
