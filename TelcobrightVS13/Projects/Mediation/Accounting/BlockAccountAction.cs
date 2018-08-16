e8637955cd405698bb3ec58b73814293edbaa0a7
Wahid Sabbir <sabbirdev@gmail.com>
2018-08-16 07:12:00 +0600

    work in progress

--- TelcobrightVS13/Projects/Mediation/Accounting/BlockAccountAction.cs
+++ TelcobrightVS13/Projects/Mediation/Accounting/BlockAccountAction.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MediationModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,7 +9,7 @@ namespace TelcobrightMediation.Accounting
 {
     public class BlockAccountAction : IAccountAction
     {
-        public bool execute()
+        public bool execute(partner partner)
         {
             throw new NotImplementedException();
         }
--- TelcobrightVS13/Projects/Mediation/Accounting/SendAlertEmailAccountAction.cs
+++ TelcobrightVS13/Projects/Mediation/Accounting/SendAlertEmailAccountAction.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using MediationModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Mail;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,9 +10,25 @@ namespace TelcobrightMediation.Accounting
 {
     public class SendAlertEmailAccountAction : IAccountAction
     {
-        public bool execute()
+        public bool execute(partner partner)
         {
-            throw new NotImplementedException();
+            SmtpClient smtpClient = new SmtpClient("mail.telcobright.com", 25);
+
+            smtpClient.Credentials = new System.Net.NetworkCredential("noreply@telcobright.com", "myPassword");
+            smtpClient.UseDefaultCredentials = true;
+            smtpClient.DeliveryMethod = SmtpDeliveryMethod.Network;
+            smtpClient.EnableSsl = true;
+            MailMessage mail = new MailMessage();
+
+            //Setting From , To and CC
+            mail.From = new MailAddress("noreply@MyWebsiteDomainName", "Telcobright Billing Portal");
+            mail.To.Add(new MailAddress(partner.email));
+            // mail.CC.Add(new MailAddress("MyEmailID@gmail.com"));
+            mail.Subject = "Account balance below threshold value";
+            mail.Body = "Dear Valued Partner, \n Our system indicates that you account balance is below threshhold value. \n\n BR \n\n Telcobright Billing Portal";
+
+            smtpClient.Send(mail);
+            return true;
         }
     }
 }
