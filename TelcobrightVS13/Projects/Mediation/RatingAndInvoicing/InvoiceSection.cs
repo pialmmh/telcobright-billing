819958e03062feae9c1fcbb83b195b84314474e1
Wahid Sabbir <sabbirdev@gmail.com>
2018-09-02 17:57:22 +0600

    2sep2018_04

---
+++ TelcobrightVS13/Projects/Mediation/RatingAndInvoicing/InvoiceSection.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TelcobrightMediation
+{
+    public class InvoiceSection
+    {
+        public string TemplateName { get; set; }
+        public String SerializedData { get; set; }
+    }
+}
--- TelcobrightVS13/Projects/_000tester/JsonCompressionTester.cs
+++ TelcobrightVS13/Projects/_000tester/JsonCompressionTester.cs
@@ -3,10 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.IO.Compression;
+using MediationModel;
+using Newtonsoft.Json;
 namespace Utils
 {
-    class JsonCompressionTester
+    public class JsonCompressionTester
     {
+        public void Test()
+        {
+            using (PartnerEntities context=new PartnerEntities())
+            {
+                var summaries = context.sum_voice_day_02.ToList();
+                string json = JsonConvert.SerializeObject(summaries);
+                Console.WriteLine($"");
+            }
+        }
     }
 }
