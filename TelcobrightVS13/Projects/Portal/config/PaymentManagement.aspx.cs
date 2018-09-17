bc3c872cb35842524004ce642bdc13a0e973a81e
Wahid Sabbir <sabbirdev@gmail.com>
2018-09-17 22:07:08 +0600

    billable changed

--- TelcobrightVS13/Projects/Portal/config/PaymentManagement.aspx.cs
+++ TelcobrightVS13/Projects/Portal/config/PaymentManagement.aspx.cs
@@ -51,7 +51,8 @@ namespace PortalApp.config
             List<KeyValuePair<Regex, string>> serviceAliases = Tbc.ServiceAliasesRegex;
             List<string> billableType = new List<string>()
             {
-                "/custBilled", "/suppBilled", "/billable"
+                //"/custBilled", "/suppBilled", "/billable"
+                "/custBilled"
             };
 
             using (PartnerEntities context = new PartnerEntities())
