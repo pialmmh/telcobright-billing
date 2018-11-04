9388b5787909749f780665df3f047c6ec95ae26f
Mustafa <mustafa@direcsource.net>
2018-11-05 00:37:22 +0600

    05Nov2018

---
+++ TelcobrightVS13/Projects/InvoiceSectionGenerator/TollFreeInvoiceSection1GeneratorWithTax.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using TelcobrightMediation;
+using System.ComponentModel.Composition;
+using MediationModel;
+using TelcobrightMediation.Accounting;
+
+namespace InvoiceSectionGenerator
+{
+    [Export("InvoiceSectionGenerator", typeof(IInvoiceSectionGenerator))]
+    public class TollFreeInvoiceSection1GeneratorWithTax : AbstractInvoiceSectionGenerator
+    {
+        public override string RuleName => this.GetType().Name;
+        public override InvoiceSection GetInvoiceSection(InvoiceSectionGeneratorData invoiceSectionGeneratorData)
+        {
+            string sql = $@"select                                                         
+                           sum(successfulcalls)	as TotalCalls,    
+                           sum(duration1)/60  as TotalMinutes,   
+                           sum(customercost)  as Amount      
+                           from {invoiceSectionGeneratorData.CdrOrSummaryTableName}                          
+                           where {invoiceSectionGeneratorData.GetWhereClauseForDateCustomerId("tup_OutPartnerId")};";
+            return base.GetInvoiceSection<InvoiceSectionDataRowForA2ZVoice>(invoiceSectionGeneratorData, sql);
+        }
+    }
+}
--- TelcobrightVS13/Projects/InvoiceSectionGenerator/TollFreeInvoiceSection3GeneratorWithTax.cs
+++ TelcobrightVS13/Projects/InvoiceSectionGenerator/TollFreeInvoiceSection3GeneratorWithTax.cs
@@ -7,23 +7,23 @@ using TelcobrightMediation.Accounting;
 namespace InvoiceSectionGenerator
 {
     [Export("InvoiceSectionGenerator", typeof(IInvoiceSectionGenerator))]
-    public class A2ZInvoiceSection3GeneratorWithTax : AbstractInvoiceSectionGenerator
+    public class TollFreeInvoiceSection3GeneratorWithTax : AbstractInvoiceSectionGenerator
     {
         public override string RuleName => this.GetType().Name;
         public override InvoiceSection GetInvoiceSection(InvoiceSectionGeneratorData invoiceSectionGeneratorData)
         {
             decimal vatPercentage = Convert.ToDecimal(invoiceSectionGeneratorData.InvoicePostProcessingData
                 .JsonDetail["vat"]);
-            string sql = $@"select tup_starttime as Date,tup_customerrate as Rate, TotalCalls,TotalMinutes, Amount,
+            string sql = $@"select tup_starttime as Date,tup_customerrate as Rate, TotalCalls,TotalMinutes, Amount
                             TaxOrVatAmount, GrandTotalAmount from
                             (select tup_starttime,tup_customerrate,
                             sum(successfulcalls)	as TotalCalls,                                  
                             sum(duration1)/60  as TotalMinutes,                                   
                             sum(customercost)  as Amount,
                             sum(customercost)*{vatPercentage} as TaxOrVatAmount,
-                            sum(customercost)*(1+{vatPercentage}) as GrandTotalAmount                                          
+                            sum(customercost)*(1+{vatPercentage}) as GrandTotalAmount                                                                                    
                             from {invoiceSectionGeneratorData.CdrOrSummaryTableName}                          
-                            where {invoiceSectionGeneratorData.GetWhereClauseForDateCustomerId("tup_inPartnerId")}
+                            where {invoiceSectionGeneratorData.GetWhereClauseForDateCustomerId("tup_OutPartnerId")}
                             group by tup_starttime,tup_customerrate) x;";
             return base.GetInvoiceSection<InvoiceSectionDataRowForA2ZVoice>(invoiceSectionGeneratorData, sql);
         }
