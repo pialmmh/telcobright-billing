394669409304045ef13a92c7cfaf4e7bdbeaf111
Telcobright Demo <sabbirdev@gmail.com>
2018-11-17 11:32:25 +0600

    work in progress

--- TelcobrightVS13/Projects/UtilInstallConfig/icnl/IcnlDirectorySetting.cs
+++ TelcobrightVS13/Projects/UtilInstallConfig/icnl/IcnlDirectorySetting.cs
@@ -23,7 +23,7 @@ namespace InstallConfig
         {
             DirectorySettings directorySettings = new DirectorySettings("Directory Settings")
             {
-                ApplicationRootDirectory = "c:/Telcobright"
+                ApplicationRootDirectory = "D:/Telcobright"
             };
             
             //***FILE LOCATIONS**********************************************
@@ -36,7 +36,7 @@ namespace InstallConfig
                 OsType = "windows",
                 PathSeparator = @"\",
                 ServerIp = "",
-                StartingPath = "C:/telcobright/Vault/Resources/CDR/Icnl/dialogic",
+                StartingPath = "D:/telcobright/Vault/Resources/CDR/Icnl/dialogic",
                 User = "",
                 Pass = "",
             };
