28b863bec42777b61a7ee4db1b1d8a7043fdad4b
JSL ICX <sabbirdev@gmail.com>
2019-03-06 01:42:58 +0600

    icx while removed from executer

--- TelcobrightVS13/Projects/_Process/CdrJobExecuter.cs
+++ TelcobrightVS13/Projects/_Process/CdrJobExecuter.cs
@@ -43,80 +43,80 @@ namespace Process
                 using (PartnerEntities context = new PartnerEntities(entityConStr))
                 {
                     context.Database.Connection.Open();
-                    var mediationContext = new MediationContext(tbc,context);
-                    while (CheckIncomplete(context, mediationContext))
+                    var mediationContext = new MediationContext(tbc, context);
+                    tbc.GetPathIndependentApplicationDirectory();
+                    foreach (ne ne in mediationContext.Nes.Values)
                     {
-                        tbc.GetPathIndependentApplicationDirectory();
-                        foreach (ne ne in mediationContext.Nes.Values)
+                        try
                         {
-                            try
+                            if (ne.SkipCdrDecoded == 1 || CheckIncomplete(context, mediationContext, ne) == false)
+                                continue;
+                            List<job> incompleteJobs = GetReProcessJobs(context, ne, ne.DecodingSpanCount);
+                            incompleteJobs.AddRange(GetNewCdrJobs(tbc, context, ne, ne.DecodingSpanCount)); //combine
+                            using (DbCommand cmd = context.Database.Connection.CreateCommand())
                             {
-                                if (ne.SkipCdrDecoded == 1 || CheckIncomplete(context, mediationContext, ne) == false)
-                                    continue;
-                                List<job> incompleteJobs = GetReProcessJobs(context, ne, ne.DecodingSpanCount);
-                                incompleteJobs.AddRange(GetNewCdrJobs(tbc,context, ne, ne.DecodingSpanCount)); //combine
-                                using (DbCommand cmd =context.Database.Connection.CreateCommand())
+                                foreach (job telcobrightJob in incompleteJobs)
                                 {
-                                    foreach (job telcobrightJob in incompleteJobs)
+                                    Console.WriteLine("Processing CdrJob for Switch:" + ne.SwitchName + ", JobName:" +
+                                                      telcobrightJob.JobName);
+                                    try
                                     {
-                                        Console.WriteLine("Processing CdrJob for Switch:" + ne.SwitchName + ", JobName:" + telcobrightJob.JobName);
-                                        try  
+                                        if (cmd.Connection.State != ConnectionState.Open) cmd.Connection.Open();
+                                        cmd.ExecuteCommandText("set autocommit=0;");
+                                        ITelcobrightJob iJob = null;
+                                        mediationContext.MefJobContainer.DicExtensionsIdJobWise.TryGetValue(
+                                            telcobrightJob.idjobdefinition.ToString(), out iJob);
+                                        if (iJob == null)
+                                            throw new Exception("JobRule not found in MEF collection.");
+                                        var cdrJobInputData =
+                                            new CdrJobInputData(mediationContext, context, ne, telcobrightJob);
+                                        iJob.Execute(cdrJobInputData); //EXECUTE
+                                        cmd.ExecuteCommandText(" commit; ");
+                                    }
+                                    catch (Exception e)
+                                    {
+                                        try
                                         {
                                             if (cmd.Connection.State != ConnectionState.Open) cmd.Connection.Open();
-                                            cmd.ExecuteCommandText("set autocommit=0;");
-                                            ITelcobrightJob iJob = null;
-                                            mediationContext.MefJobContainer.DicExtensionsIdJobWise.TryGetValue(
-                                                telcobrightJob.idjobdefinition.ToString(), out iJob);
-                                            if (iJob == null)
-                                                throw new Exception("JobRule not found in MEF collection.");
-                                            var cdrJobInputData =
-                                                new CdrJobInputData(mediationContext, context, ne, telcobrightJob);
-                                            iJob.Execute(cdrJobInputData); //EXECUTE
-                                            cmd.ExecuteCommandText(" commit; ");
-                                        }
-                                        catch (Exception e)
-                                        {
+                                            cmd.ExecuteCommandText(" rollback; ");
+                                            bool cacheLimitExceeded =
+                                                RateCacheCleaner.CheckAndClearRateCache(mediationContext, e);
+                                            if (cacheLimitExceeded) continue;
+                                            cacheLimitExceeded = RateCacheCleaner.ClearTempRateTable(e, cmd);
+                                            if (cacheLimitExceeded) continue;
+                                            PrintErrorMessageToConsole(ne, telcobrightJob, e);
+                                            ErrorWriter wr = new ErrorWriter(e, "ProcessCdr", telcobrightJob,
+                                                "CdrJob processing error.", tbc.DatabaseSetting.DatabaseName);
                                             try
                                             {
-                                                if (cmd.Connection.State != ConnectionState.Open) cmd.Connection.Open();
-                                                cmd.ExecuteCommandText(" rollback; ");
-                                                bool cacheLimitExceeded = RateCacheCleaner.CheckAndClearRateCache(mediationContext, e);
-                                                if (cacheLimitExceeded) continue;
-                                                cacheLimitExceeded = RateCacheCleaner.ClearTempRateTable(e,cmd);
-                                                if (cacheLimitExceeded) continue;
-                                                PrintErrorMessageToConsole(ne, telcobrightJob, e);
-                                                ErrorWriter wr = new ErrorWriter(e, "ProcessCdr", telcobrightJob,
-                                                    "CdrJob processing error.", tbc.DatabaseSetting.DatabaseName);
-                                                try
-                                                {
-                                                    UpdateJobWithErrorInfo(cmd, telcobrightJob, e);
-                                                }
-                                                catch (Exception e2)
-                                                {
-                                                    ErrorWriter wr2 = new ErrorWriter(e2, "ProcessCdr", telcobrightJob,
-                                                        "Exception within catch block.",
-                                                        tbc.DatabaseSetting.DatabaseName);
-                                                }
-                                                continue; //with next cdr or job
+                                                UpdateJobWithErrorInfo(cmd, telcobrightJob, e);
                                             }
-                                            catch (Exception)
+                                            catch (Exception e2)
                                             {
-                                                //reaching here would be database problem
-                                                context.Database.Connection.Close();
+                                                ErrorWriter wr2 = new ErrorWriter(e2, "ProcessCdr", telcobrightJob,
+                                                    "Exception within catch block.",
+                                                    tbc.DatabaseSetting.DatabaseName);
                                             }
-                                        } //end catch
-                                    } //for each job
-                                } //using mysql command
-                            } //try for each NE
+                                            continue; //with next cdr or job
+                                        }
+                                        catch (Exception)
+                                        {
+                                            //reaching here would be database problem
+                                            context.Database.Connection.Close();
+                                        }
+                                    } //end catch
+                                } //for each job
+                            } //using mysql command
+                        } //try for each NE
 
-                            catch (Exception e1)
-                            {
-                                Console.WriteLine(e1);
-                                ErrorWriter wr = new ErrorWriter(e1, "ProcessCdr", null, "NE:" + ne.idSwitch,tbc.DatabaseSetting.DatabaseName);
-                                continue; //with next switch
-                            }
-                        } //for each NE
-                    } //while
+                        catch (Exception e1)
+                        {
+                            Console.WriteLine(e1);
+                            ErrorWriter wr = new ErrorWriter(e1, "ProcessCdr", null, "NE:" + ne.idSwitch,
+                                tbc.DatabaseSetting.DatabaseName);
+                            continue; //with next switch
+                        }
+                    } //for each NE
                 }
             } //try
             catch (Exception e1)
