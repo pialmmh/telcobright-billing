78a3e4607213d4578f34e16e310675c037bb6db7
Wahid Sabbir <sabbirdev@gmail.com>
2019-06-29 20:25:43 +0600

    [wip] sansay switch prepaid blocking

--- TelcobrightVS13/Projects/mefSwitchOperation/Sansay/SansaySwitch.cs
+++ TelcobrightVS13/Projects/mefSwitchOperation/Sansay/SansaySwitch.cs
@@ -10,7 +10,7 @@ using TelcobrightMediation.SansayWs;
 
 namespace TelcobrightMediation.Sansay
 {
-    public class SansaySwitch
+    public class SansaySwitch : ISwitchAction
     {
         private string _protocol;
         private string _ipAddress;
@@ -29,8 +29,9 @@ namespace TelcobrightMediation.Sansay
             _wsPort = new SansayWSClient("SansayWSPort", new EndpointAddress($"{protocol}://{ipAddress}:{port}/SSConfig/SansayWS"));
         }
 
-        public bool DownloadXmlFile(string table, string destinationDirectory)
+        public List<string> DownloadXmlFile(string table, string destinationDirectory)
         {
+            List<string> files = new List<string>();
             doDownloadXmlFileResponse result;
             int index = 0;
             do
@@ -42,7 +43,7 @@ namespace TelcobrightMediation.Sansay
                     password = _password,
                     page = index
                 };
-                string xmlFileName = $"{destinationDirectory}\\{table}_{index++}.xml";
+                string xmlFileName = Path.Combine(destinationDirectory, $"{table}_{index++}.xml");
                 doDownloadXmlFileRequest drequest = new doDownloadXmlFileRequest(dparams);
                 result = _wsPort.doDownloadXmlFile(drequest);
 
@@ -51,11 +52,29 @@ namespace TelcobrightMediation.Sansay
                     StreamWriter file = new StreamWriter(xmlFileName);
                     file.Write(result.downloadResult.xmlfile);
                     file.Close();
+                    files.Add(xmlFileName);
                 }
-                else return false;
+                else throw new Exception(result.downloadResult.msg);
 
             } while (result.downloadResult.hasMore == 1);
-            return true;
+            return files;
+        }
+
+        public bool UploadXmlFile(string table, string xmlFileName)
+        {
+            if (!File.Exists(xmlFileName)) throw new Exception("File do not exists.");
+            string xmlfile = File.ReadAllText(xmlFileName);
+            uploadParams uparams = new uploadParams()
+            {
+                table = table,
+                username = _username,
+                password = _password,
+                xmlfile = xmlfile
+            };
+            doUploadXmlFileRequest request = new doUploadXmlFileRequest(uparams);
+            doUploadXmlFileResponse response = _wsPort.doUploadXmlFile(request);
+            if (response.uploadResult.retCode == (int) SansayWsReturnCodes.Ok) return true;
+            else throw new Exception(response.uploadResult.msg);
         }
 
         private List<String> GetAvailableTrunks(XDocument document)
@@ -72,6 +91,36 @@ namespace TelcobrightMediation.Sansay
         {
             XElement element = document.Descendants("XBResource").First(x => x.Descendants("trunkId").First().Value == trunkId);
             return element.Descendants("capacity").First().Value;
+        }
+
+        private XDocument GetFileContent(string xmlFileName)
+        {
+            if (!File.Exists(xmlFileName)) throw new Exception("File do not exists.");
+            return XDocument.Load(xmlFileName);
+        }
+
+        private XDocument ChangeCapacity(XDocument document, string trunkId, string newCapacity)
+        {
+            XElement element = document.Descendants("XBResource").First(x => x.Descendants("trunkId").First().Value == trunkId);
+            element.Descendants("capacity").First().Value = newCapacity;
+            return document;
+        }
+
+        public bool Block(string trunkId)
+        {
+            string table = "resource";
+            List<string> files = this.DownloadXmlFile(table, string.Empty);
+            if (files.Count > 0)
+            {
+                foreach (string file in files)
+                {
+                    XDocument document = this.ChangeCapacity(this.GetFileContent(file), trunkId, "0");
+                    document.Save(file);
+                    this.UploadXmlFile(table, file);
+                }
+                return true;
+            }
+            return false;
         }
     }
 }
