9c35bac3af5e5dc277449cc0d5cbef7e0f54f413
mustafa_r <mailto:mustafa@directroutes.net>
2023-09-14 15:18:33 +0600

    merged test

--- TelcobrightVS13/Projects/CasTelcobright/StatusPictureBox.cs
+++ TelcobrightVS13/Projects/CasTelcobright/StatusPictureBox.cs
@@ -10,14 +10,8 @@ namespace CasTelcobright
 {
     public static class StatusColors
     {
-<<<<<<< HEAD
-        public static Color StatusOn = Color.LimeGreen;
-        public static Color StatusOff = Color.DarkGray;
-        public static  Color StatusException = Color.DarkOrange;
-=======
         public static readonly Color StatusOn = Color.LimeGreen;
         public static readonly Color StatusOff = Color.DarkGray;
         public static readonly Color StatusException = Color.DarkOrange;
->>>>>>> a341a24c1fdc2a61116133492f415135cb69eafc
     }
 }
