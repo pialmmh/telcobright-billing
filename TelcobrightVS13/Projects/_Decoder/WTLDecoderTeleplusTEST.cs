660a5f2b068ae5677c787bf14ee33ecc2c9c71a3
mustafa_r <mustafa@directroutes.net>
2023-09-21 20:40:43 +0600

    start wtl decoder

---
+++ TelcobrightVS13/Projects/_Decoder/WTLDecoderTeleplusTEST.cs
@@ -0,0 +1,70 @@
+﻿using TelcobrightMediation;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using TelcobrightFileOperations;
+using MediationModel;
+using TelcobrightMediation.Cdr;
+using TelcobrightMediation.Mediation.Cdr;
+using System.Linq;
+using System.Globalization;
+using System.IO;
+using LibraryExtensions;
+
+namespace Decoders
+{
+
+    [Export("Decoder", typeof(IFileDecoder))]
+    public class WTLDecoderTeleplusTEST : IFileDecoder
+    {
+        public override string ToString() => this.RuleName;
+        public virtual string RuleName => GetType().Name;
+        public int Id => 55;
+        public string HelpText => "WTL Decoder Teleplus TEST";
+        public CompressionType CompressionType { get; set; }
+        protected CdrCollectorInputData Input { get; set; }
+
+
+        private static DateTime parseStringToDate(string timestamp)  //20181028051316400 yyyyMMddhhmmssfff
+        {
+            DateTime dateTime = DateTime.ParseExact(timestamp, "yyyyMMddHHmmssfff", CultureInfo.InvariantCulture);
+            return dateTime;
+        }
+
+        public List<string[]> DecodeFile(CdrCollectorInputData input, out List<cdrinconsistent> inconsistentCdrs)
+        {
+            this.Input = input;
+            string fileName = this.Input.FullPath;
+            //List<string[]> lines = FileUtil.ParseCsvWithEnclosedAndUnenclosedFields(fileName, ',', 1, "\"", ";");
+            List<string[]> lines = FileUtil.ParseCsvWithEnclosedAndUnenclosedFields(fileName, ',', 1, "\"", ";");
+
+            inconsistentCdrs = new List<cdrinconsistent>();
+            List<string[]> decodedRows = new List<string[]>();
+            //this.Input = input;
+            List<cdrfieldmappingbyswitchtype> fieldMappings = null;
+
+            foreach (string[] lineAsArr in lines)
+            {
+                //string chargingStatus = lineAsArr[3] == "S" ? "1" : "0"; //done
+                //if (chargingStatus != "1") continue;
+                string[] textCdr = new string[input.MefDecodersData.Totalfieldtelcobright];
+                textCdr[Fn.OriginatingCallingNumber] = 
+                textCdr[Fn.Validflag] = "1";
+                decodedRows.Add(textCdr);
+            }
+
+            return decodedRows;
+
+        }
+
+        public string getTupleExpression(CdrCollectorInputData decoderInputData, string[] row)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string getSqlWhereClauseForDayWiseSafeCollection(CdrCollectorInputData decoderInputData, DateTime day)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
