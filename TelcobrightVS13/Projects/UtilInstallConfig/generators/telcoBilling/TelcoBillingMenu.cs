8a996b17214958da5a37b05861ebcb834860bbe0
mustafa_r <mustafa@directroutes.net>
2023-10-01 06:08:32 +0600

    noCleaningDeployedInstances

--- TelcobrightVS13/Projects/UtilInstallConfig/generators/telcoBilling/TelcoBillingMenu.cs
+++ TelcobrightVS13/Projects/UtilInstallConfig/generators/telcoBilling/TelcoBillingMenu.cs
@@ -237,7 +237,7 @@ namespace InstallConfig
         void generateConfig()
         {
             List<TelcobrightConfig> selectedTbcs = getSelectedTbcs();
-            cleanDeploymentDir();
+            //cleanDeploymentDir();
             foreach (var tbWithoutFullConfig in selectedTbcs)
             {
                 var dbOrInstanceName = tbWithoutFullConfig.Telcobrightpartner.databasename;
