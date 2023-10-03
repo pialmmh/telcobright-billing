10c01db91b4f9afb5d710fb1b290da2d6df99720
mustafa_r <mustafa@directroutes.net>
2023-10-03 20:58:35 +0600

    something change

--- TelcobrightVS13/Projects/_Decoder/CataleyaCsvDecoderBantelTEST.cs
+++ TelcobrightVS13/Projects/_Decoder/CataleyaCsvDecoderBantelTEST.cs
@@ -127,7 +127,7 @@ namespace Decoders
             throw new NotImplementedException();
         }
 
-        public string getSqlWhereClauseForHourWiseSafeCollection(CdrCollectorInputData decoderInputData, DateTime day)
+        public string getSqlWhereClauseForHourWiseSafeCollection(CdrCollectorInputData decoderInputData, DateTime hourOfDay,int minusHoursForSafeCollection, int plusHoursForSafeCollection)
         {
             throw new NotImplementedException();
         }
