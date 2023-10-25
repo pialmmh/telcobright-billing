0d2e7bd2682f6fea3a6b2405ee989253e14a1e82
mustafa_r <mustafa@directroutes.net>
2023-10-25 18:16:09 +0600

    preProcessorInProgress

--- TelcobrightVS13/Projects/UtilInstallConfig/config/allDeployment/casTemplate.cs
+++ TelcobrightVS13/Projects/UtilInstallConfig/config/allDeployment/casTemplate.cs
@@ -10,103 +10,13 @@ namespace InstallConfig
 {
     public static partial class AllDeploymenProfiles
     {
-        public static List<Deploymentprofile> getDeploymentprofiles()
+        public static List<Deploymentprofile> getLinux01DeploymentProfile()
         {
             TelcobrightSeed seed = new TelcobrightSeed();
             List<Deploymentprofile> allProfiles =
                 new List<Deploymentprofile>
                 {
-                    new Deploymentprofile
-                    {
-                        profileName = "srtelecom",
-                        type = DeploymentProfileType.TelcoBilling,
-                        instances = new List<InstanceConfig>
-                        {
-                            new InstanceConfig
-                            {
-                                Name = "srtelecom",
-                                SchedulerPortNo = 555,
-                            },
-                        }
-                    },
-                    new Deploymentprofile
-                    {
-                        profileName = "summit",
-                        type = DeploymentProfileType.TelcoBilling,
-                        instances = new List<InstanceConfig>
-                        {
-                            new InstanceConfig
-                            {
-                                Name = "summit",
-                                SchedulerPortNo = 556
-                            },
-                        },
-                    },
-                    new Deploymentprofile
-                    {
-                        profileName = "banglatelecom",
-                        type = DeploymentProfileType.TelcoBilling,
-                        instances = new List<InstanceConfig>
-                        {
-                            new InstanceConfig
-                            {
-                                Name = "banglatelecom",
-                                SchedulerPortNo = 557
-                            },
-                        }
-                    },
-                    new Deploymentprofile
-                    {
-                        profileName = "jsl",
-                        type = DeploymentProfileType.TelcoBilling,
-                        instances = new List<InstanceConfig>
-                        {
-                            new InstanceConfig
-                            {
-                                Name = "jsl",
-                                SchedulerPortNo = 558
-                            },
-                        }
-                    },
-                    new Deploymentprofile
-                    {
-                        profileName = "purple",
-                        type = DeploymentProfileType.TelcoBilling,
-                        instances = new List<InstanceConfig>
-                        {
-                            new InstanceConfig
-                            {
-                                Name = "purple",
-                                SchedulerPortNo = 559
-                            },
-                        }
-                    },
-                    new Deploymentprofile
-                    {
-                        profileName = "btel",
-                        type = DeploymentProfileType.TelcoBilling,
-                        instances = new List<InstanceConfig>
-                        {
-                            new InstanceConfig
-                            {
-                                Name = "btel",
-                                SchedulerPortNo = 560
-                            },
-                        }
-                    },
-                    new Deploymentprofile
-                    {
-                        profileName = "dbl",
-                        type = DeploymentProfileType.TelcoBilling,
-                        instances = new List<InstanceConfig>
-                        {
-                            new InstanceConfig
-                            {
-                                Name = "dbl",
-                                SchedulerPortNo = 561
-                            },
-                        }
-                    }
+                    
                 };
 
             var deploymentprofile = new Deploymentprofile
@@ -262,12 +172,12 @@ namespace InstallConfig
                 },
                 MySqlCluster = new MySqlCluster
                 {
-                    Master = new MySqlServer("master_win12")
+                    Master = new MySqlServer("linux01docker20")
                     {
                         MySqlVersion = MySqlVersion.MySql57,
                         BindAddressForAutomation = new BindAddress
                         {
-                            IpAddressOrHostName = new IpAddressOrHostName {Address = "localhost"},
+                            IpAddressOrHostName = new IpAddressOrHostName {Address = "localhost"},//10.12.1.1
                             Port = 3306
                         },
                         RootUserForAutomation = "btrc",
@@ -443,53 +353,7 @@ namespace InstallConfig
                     Slaves = new List<MySqlServer>()
                 }
             };
-            allProfiles.Add(deploymentprofile);
-            var tcpPortWisecount = allProfiles.SelectMany(p => p.instances.Select(i => i.SchedulerPortNo))
-                .GroupBy(portNo => portNo)
-                .Select(g => new
-                {
-                    tcpPort = g.Key,
-                    count = g.Count()
-                }).Where(a => a.count > 1).ToList();
-
-            foreach (var a in tcpPortWisecount)
-            {
-                if (a.count > 1)
-                {
-                    throw new Exception("Scheduler port numbers must be unique, duplicate port no: " + a.tcpPort);
-                }
-            }
-            var profileNameCount= allProfiles.Select(p=>p.profileName)
-                .GroupBy(name=> name)
-                .Select(g => new
-                {
-                    name=g.Key,
-                    count = g.Count()
-                }).Where(a => a.count > 1);
-
-            foreach (var a in profileNameCount)
-            {
-                if (a.count > 1)
-                {
-                    throw new Exception("Profile names must be unique, duplicate name: " + a.name);
-                }
-            }
-
-            var instanceOrDatabaseName = allProfiles.SelectMany(p => p.instances.Select(i=>i.Name))
-                .GroupBy(name => name)
-                .Select(g => new
-                {
-                    name = g.Key,
-                    count = g.Count()
-                }).Where(a => a.count > 1);
-
-            foreach (var a in instanceOrDatabaseName)
-            {
-                if (a.count > 1)
-                {
-                    throw new Exception("Instance or database names must be unique, duplicate name: " + a.name);
-                }
-            }
+            
             return allProfiles;
         }
     }
