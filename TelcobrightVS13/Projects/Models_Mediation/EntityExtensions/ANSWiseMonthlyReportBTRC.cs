bbce07e3906ea7b8459b061492c7969d4cef23f9
mustafa_r <mustafa@directroutes.net>
2024-01-24 19:22:10 +0600

    ans wise monthly report added

---
+++ TelcobrightVS13/Projects/Models_Mediation/EntityExtensions/ANSWiseMonthlyReportBTRC.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MediationModel
+{
+    public partial class ANSWiseMonthlyReportBTRC
+    {
+        public string PartnerName { get; set; }
+        public decimal? DomIn { get; set; }
+        public decimal? DomOut { get; set; }
+        public decimal? IntIn { get; set; }
+        public decimal? IntOut { get; set; }
+    }
+ }
+
