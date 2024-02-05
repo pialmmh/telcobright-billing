2c6e076da44059ad87dc99997ef1368cb73491bd
mustafa_r <mustafa@directroutes.net>
2024-02-05 20:37:39 +0600

    foreignkey bug fixed in rateplan assignment

--- TelcobrightVS13/Projects/LibraryExtensions/DbWriterWithAccurateCount.cs
+++ TelcobrightVS13/Projects/LibraryExtensions/DbWriterWithAccurateCount.cs
@@ -14,25 +14,25 @@ namespace LibraryExtensions
         public static string DefaultSpForMultipleStatements { get; set; } = "sp_exec_multiple_statement";
         public static int ExecSingleStatementThroughStoredProc(DbCommand dbCmd, string command, int expectedRecCount)
         {
-            dbCmd.CommandText = DefaultSpForSingleStatement;
-            dbCmd.CommandType = CommandType.StoredProcedure;
+                dbCmd.CommandText = DefaultSpForSingleStatement;
+                dbCmd.CommandType = CommandType.StoredProcedure;
 
-            DbParameter param1 = dbCmd.CreateParameter();
-            param1.ParameterName = "@command";
-            param1.Value = command.EndsWith(";") ? command : new StringBuilder(command).Append(";").ToString();
-            dbCmd.Parameters.Add(param1);
+                DbParameter param1 = dbCmd.CreateParameter();
+                param1.ParameterName = "@command";
+                param1.Value = command.EndsWith(";") ? command : new StringBuilder(command).Append(";").ToString();
+                dbCmd.Parameters.Add(param1);
 
-            DbParameter param2 = dbCmd.CreateParameter();
-            param2.ParameterName = "@expectedRecCount";
-            param2.Value = expectedRecCount;
-            dbCmd.Parameters.Add(param2);
+                DbParameter param2 = dbCmd.CreateParameter();
+                param2.ParameterName = "@expectedRecCount";
+                param2.Value = expectedRecCount;
+                dbCmd.Parameters.Add(param2);
 
-            int affectedRecordCount = (int)dbCmd.ExecuteScalar();
-            dbCmd.Parameters.Clear();
-            dbCmd.CommandType = CommandType.Text;
-            return affectedRecordCount;
+                int affectedRecordCount = (int)dbCmd.ExecuteScalar();
+                dbCmd.Parameters.Clear();
+                dbCmd.CommandType = CommandType.Text;
+                return affectedRecordCount;
         }
-      
+
         public static int ExecMultipleStatementsThroughStoredProc(DbCommand dbCmd, List<string> commands, int expectedRecCount)
         {
             dbCmd.CommandType = CommandType.Text;
