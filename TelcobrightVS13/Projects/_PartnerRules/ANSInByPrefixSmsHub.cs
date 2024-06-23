a2e56ddecfc9608c7ed86224a289a68736691331
Mikdad00000 <mikdad00000@gmail.com>
2024-06-23 09:22:04 +0600

    new partnerRules added for smshub (Ans In/out by prefix)

---
+++ TelcobrightVS13/Projects/_PartnerRules/ANSInByPrefixSmsHub.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using TelcobrightMediation;
+using System.ComponentModel.Composition;
+using System.Linq;
+using MediationModel;
+
+namespace PartnerRules
+{
+
+    [Export("Partner", typeof(IPartnerRule))]
+    public class ANSInByPrefixSmsHub : IPartnerRule
+    {
+        public override string ToString()
+        {
+            return this.RuleName;
+        }
+        public string RuleName => GetType().Name;
+        public string HelpText => "Ingress Partner Identification by incoming partner prefix ANSInByPrefixSmsHub";
+        public int Id => 10;
+        public int Execute(cdr thisCdr, MefPartnerRulesContainer data)
+        {
+            Dictionary<string, partnerprefix> ansPrefixes880 = data.MediationContext.AnsPrefixes880;
+            thisCdr.InPartnerId = 0;
+            string originatingCallingNumber = thisCdr.OriginatingCallingNumber;
+
+            foreach (KeyValuePair<string, partnerprefix> kv in ansPrefixes880)
+            {
+                string prefix = kv.Key;
+                partnerprefix ansPrefix = kv.Value;
+
+                if (originatingCallingNumber.StartsWith(prefix))
+                {
+                    thisCdr.InPartnerId = ansPrefix.idPartner;
+                    return ansPrefix.idPartner;
+                }
+            }
+            return 0;
+        }
+    }
+}
---
+++ TelcobrightVS13/Projects/_PartnerRules/ANSOutByPrefixSmsHub.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using TelcobrightMediation;
+using System.ComponentModel.Composition;
+using System.Linq;
+using MediationModel;
+
+namespace PartnerRules
+{
+
+    [Export("Partner", typeof(IPartnerRule))]
+    public class ANSOutByPrefixSmsHub : IPartnerRule
+    {
+        public override string ToString()
+        {
+            return this.RuleName;
+        }
+        public string RuleName => GetType().Name;
+        public string HelpText => "Egress Partner Identification by outgoing prefix, ANSOutByPrefixSmsHub";
+        public int Id => 11;
+        public int Execute(cdr thisCdr, MefPartnerRulesContainer data)
+        {
+            Dictionary<string, partnerprefix> ansPrefixes880 = data.MediationContext.AnsPrefixes880;
+            thisCdr.OutPartnerId = 0;
+            string originatingCalledNumber = thisCdr.OriginatingCalledNumber;
+
+            foreach (KeyValuePair<string, partnerprefix> kv in ansPrefixes880)
+            {
+                string prefix = kv.Key;
+                partnerprefix ansPrefix = kv.Value;
+
+                if (originatingCalledNumber.StartsWith(prefix))
+                {
+                    thisCdr.OutPartnerId = ansPrefix.idPartner;
+                    return ansPrefix.idPartner;
+                }
+            }
+            return 0;
+        }
+    }
+}
