46ac3ed7f1007d06d250c617979726b6bf546959
Mikdad00000 <mikdad00000@gmail.com>
2024-06-23 09:37:17 +0600

    mdr commonValidationRules for smshub;

---
+++ TelcobrightVS13/Projects/UtilInstallConfig/util/CommonValidationRulesGenerator/CommonMdrValRulesGen.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CdrValidationRules;
+using CdrValidationRules.CdrValidationRules.CommonCdrValidationRules;
+using LibraryExtensions;
+using MediationModel;
+using TelcobrightMediation;
+
+namespace InstallConfig._CommonValidation
+{
+    public class CommonMdrValRulesGen : CommonCdrValRulesGen
+    {
+        private DateTime NotAllowedCallDateTimeBefore { get; }
+
+        public CommonMdrValRulesGen(DateTime notAllowedCallDateTimeBefore) 
+            : base(notAllowedCallDateTimeBefore)
+        {
+            this.NotAllowedCallDateTimeBefore = notAllowedCallDateTimeBefore;
+        }
+
+        public override List<IValidationRule<cdr>> GetRules()
+        {
+            return new List<IValidationRule<cdr>>()
+            {
+                new UniqueBillIdNonEmpty(),
+                //new SeqNumGreaterThanZero(),
+                new PartialFlagGtEq0(),
+                new IncomingRouteNotEmpty(),
+                new OriginatingCalledNumberNotEmpty(),
+                new DurationSecGtEq0(),
+                new StartTimeIsValidAndWithinLimit()
+                {
+                    Data = this.NotAllowedCallDateTimeBefore
+                },
+                //new EndTimeIsGtEqStartTime(),
+                new ValidFlagGt0(),
+                new SwitchIdGt0(),
+                new IdCallGt0(),
+                new FileNameNotEmpty(),
+                //new InPartnerIdGt0(),
+                //new FinalRecordMustBe1(),
+                //new ChargingStatus1WhenDurationGt0(),
+             };
+        }
+    }
+}
---
+++ TelcobrightVS13/Projects/UtilInstallConfig/util/CommonValidationRulesGenerator/InconsistentMdrValRulesGen.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CdrValidationRules;
+using LibraryExtensions;
+using TelcobrightMediation;
+
+namespace InstallConfig._CommonValidation
+{
+    public class InconsistentMdrValRulesGen: InconsistentCdrValRulesGen
+    {
+        private DateTime NotAllowedCallDateTimeBefore { get; }
+
+        public InconsistentMdrValRulesGen(DateTime notAllowedCallDateTimeBefore)
+            :base(notAllowedCallDateTimeBefore)
+        {
+            this.NotAllowedCallDateTimeBefore = notAllowedCallDateTimeBefore;
+        }
+
+        public override List<IValidationRule<string[]>> GetRules()
+        {
+            return new List<IValidationRule<string[]>>()
+            {
+                new StrUniqueBillidNotEmpty(),
+                //new StrSeqNumGreaterThanZero(),
+                new StrIncomingRouteNotEmpty(),
+                
+                new StrOriginatingCalledNumberNotEmpty(),
+                new StrOriginatingCallingNumberNotEmpty(),
+                new StrDurationSecGtEq0(),
+                new StrStartTimeIsValidAndWithinLimit()
+                {
+                    Data = this.NotAllowedCallDateTimeBefore
+                },
+                //new StrEndTimeIsGtEqStartTime(),
+                new StrValidFlagMustBe1()
+            };
+        }
+    }
+}
