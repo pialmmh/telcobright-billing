97c01aea6dff7faaf3c4d070c565d9badf3f8f57
Mikdad00000 <mikdad00000@gmail.com>
2024-07-24 15:13:17 +0600

    smshubservicegroup config updated

--- TelcobrightVS13/Projects/UtilInstallConfig/config/smshub/SmsHubServiceGroups.cs
+++ TelcobrightVS13/Projects/UtilInstallConfig/config/smshub/SmsHubServiceGroups.cs
@@ -24,91 +24,12 @@ namespace InstallConfig
         public Dictionary<int, ServiceGroupConfiguration> GetServiceGroupConfigurations()
         {
             List<ServiceGroupConfiguration> serviceGroupConfigurations = new List<ServiceGroupConfiguration>();
-
-            serviceGroupConfigurations.Add(new ServiceGroupConfiguration(idServiceGroup: 6) //LTFS
-            {
-                Params = new Dictionary<string, string>() { {"prefixes","0800" } },
-                PartnerRules = new List<int>()
-                {
-                    PartnerRuletype.InPartnerByIncomingRoute,
-                    PartnerRuletype.OutPartnerByOutgoingRoute
-                },
-                Ratingtrules = new List<RatingRule>()
-                {
-                    new RatingRule() {IdServiceFamily = ServiceFamilyType.SfTollFreeEgressCharging, AssignDirection = 2},
-                },
-                MediationChecklistForAnsweredCdrs =
-                    new List<IValidationRule<cdr>>()
-                    {
-                        new DurationSecGtEq0(),
-                        new OutgoingRouteNotEmpty(),
-                        new InPartnerIdGt0(),
-                        new OutPartnerIdGt0(),
-                        new ServiceGroupGt0(),
-                        new MatchedPrefixSupplierNotEmpty(),
-                        new Duration2Gt0() {Data = .1M},
-                        new OutPartnerCostGt0() {Data = .1M},
-                        new BtrcRevShareTax1Gt0() {Data = .1M},
-                     },
-                InvoiceGenerationConfig = new InvoiceGenerationConfig()
-                {
-                    InvoiceGenerationRuleName = "InvoiceGenerationByLedgerSummary",
-                    SectionGeneratorVsTemplateNames = new Dictionary<string, string>()
-                    {
-                        {"TollFreeInvoiceSection1GeneratorWithTax","LTFSToIPTSP" },
-                        {"TollFreeInvoiceSection2GeneratorWithTax","LTFSToIPTSPDetails1" },
-                        {"TollFreeInvoiceSection3GeneratorWithTax","LTFSToIPTSPDetails2" }
-                    }
-                }
-            });
-
-            serviceGroupConfigurations.Add(new ServiceGroupConfiguration(idServiceGroup: 2) //intlOutIcx
-            {
-                Params = new Dictionary<string, string>()
-                { { "idCdrRules", "2" } },//IcxOutgoingCallByInOutTg=2, IcxOutgoingCallByInTgType=1
-                PartnerRules = new List<int>()
-                {
-                    PartnerRuletype.InPartnerByIncomingRoute,
-                    PartnerRuletype.OutPartnerByOutgoingRoute
-                },
-                Ratingtrules = new List<RatingRule>()
-                {
-                    new RatingRule() {IdServiceFamily = ServiceFamilyType.XyzIcx, AssignDirection = 0}
-                },
-                MediationChecklistForAnsweredCdrs =
-                    new List<IValidationRule<cdr>>()
-                    {
-                        new DurationSecGtEq0(),
-                        new OutgoingRouteNotEmpty(),
-                        new InPartnerIdGt0(),
-                        new OutPartnerIdGt0(),
-                        new ServiceGroupGt0(),
-                        new MatchedPrefixYNotempty(),
-                        new CountryCodeNotEmpty(),
-                        new RevIcxOutNot0() {Data = .1M},
-                        new BtrcRevShareTax2Gt0(){Data = .1M},
-                        new XAmountGt0(){Data = .1M},
-                        new YAmountGt0(){Data = .1M},
-                        new RoundedDurationGt0(){Data = .1M}
-                    },
-                InvoiceGenerationConfig = new InvoiceGenerationConfig()
-                {
-                    InvoiceGenerationRuleName = "InvoiceGenerationByLedgerSummary",
-                    SectionGeneratorVsTemplateNames = new Dictionary<string, string>()
-                    {
-                        {"XyzSection1GeneratorWithTax","InternationalOutgoingToANS" },
-                        {"XyzSection2GeneratorWithTax","InternationalOutgoingToANSDetails1" },
-                        {"XyzSection3GeneratorWithTax","InternationalOutgoingToANSDetails2" }
-                    }
-                }
-            });
-
             serviceGroupConfigurations.Add(new ServiceGroupConfiguration(idServiceGroup: 1) //domestic
             {
                 PartnerRules = new List<int>()
                 {
-                    PartnerRuletype.InPartnerByIncomingRoute,
-                    PartnerRuletype.OutPartnerByOutgoingRoute
+                    PartnerRuletype.InAnsByPrefixSmsHub,
+                    PartnerRuletype.OutAnsByPrefixSmsHub
                 },
                 Ratingtrules = new List<RatingRule>()
                 {
@@ -118,7 +39,7 @@ namespace InstallConfig
                     new List<IValidationRule<cdr>>()
                     {
                         new DurationSecGtEq0(),
-                        new OutgoingRouteNotEmpty(),
+                        //new OutgoingRouteNotEmpty(),
                         new InPartnerIdGt0(),
                         new OutPartnerIdGt0(),
                         new ServiceGroupGt0(),
@@ -138,42 +59,7 @@ namespace InstallConfig
                     }
                 }
             });
-            serviceGroupConfigurations.Add(
-                new ServiceGroupConfiguration(idServiceGroup: 3) //intlInIcx
-                {
-                    PartnerRules = new List<int>()
-                    {
-                        PartnerRuletype.InPartnerByIncomingRoute,
-                        PartnerRuletype.OutPartnerByOutgoingRoute
-                    },
-                    Ratingtrules = new List<RatingRule>()
-                    {
-                        new RatingRule() {IdServiceFamily = ServiceFamilyType.A2Z, AssignDirection = 1},
-                    },
-                    MediationChecklistForAnsweredCdrs =
-                        new List<IValidationRule<cdr>>()
-                        {
-                            new DurationSecGtEq0(),
-                            new OutgoingRouteNotEmpty(),
-                            new InPartnerIdGt0(),
-                            new OutPartnerIdGt0(),
-                            new ServiceGroupGt0(),
-                            new MatchedPrefixCustomerNotEmpty(),
-                            new Duration1Gt0() {Data = .1M},
-                            new InPartnerCostGt0() {Data = .1M},
-                            new BtrcRevShareTax1Gt0() {Data = .1M},
-                        },
-                    InvoiceGenerationConfig = new InvoiceGenerationConfig()
-                    {
-                        InvoiceGenerationRuleName = "InvoiceGenerationByLedgerSummary",
-                        SectionGeneratorVsTemplateNames = new Dictionary<string, string>()
-                        {
-                            {"A2ZInvoiceSection1GeneratorWithCurrencyConversion","InternationalToIOS" },
-                            {"A2ZInvoiceSection2GeneratorWithCurrencyConversion","InternationalToIOSDetails1" },
-                            {"A2ZInvoiceSection3GeneratorWithCurrencyConversion","InternationalToIOSDetails2" }
-                        }
-                    }
-                });
+            
             return serviceGroupConfigurations.ToDictionary(s => s.IdServiceGroup);
         }
     }
